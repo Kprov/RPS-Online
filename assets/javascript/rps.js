516d2af92410cdad8ab4c8d7a46558402744bdcb
Mr_Cheese <kevinprovost776@gmail.com>
2019-01-24 04:27:21 -0600

    forms completed

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -5,15 +5,6 @@ $(document).ready(function(){
         var wins = 0;
         var losses = 0;
         var ties = 0;
-        var userChoices = ["Rock", "Paper", "Scissors"];
-        var buttonDiv = $("<div>");
         
-        userChoices.forEach(display);
-        buttonDiv += "</div>";
-        document.getElementById("btnz").innerHTML = buttonDiv;
-
-        function display(value) {
-            buttonDiv += "<button class='button btn-lg btn-primary buttons text-center'>" + value + "</button>";
-        }
         
 })
